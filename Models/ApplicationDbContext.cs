9b8231eb15c0350c69ef6696ece50c9a3361e760
xxal <leckalmeida@gmail.com>
2024-07-02 17:11:05 -0300

    :tada: Commit inicial

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Transactions;
+
+namespace ExpenseTracker.Models {
+    public class ApplicationDbContext : DbContext {
+
+        public ApplicationDbContext(DbContextOptions options):base(options) {
+            
+        }
+
+        public DbSet <Transaction> Transactions { get; set; }
+        public DbSet <Category> Categories { get; set; }
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ExpenseTracker.Models {
+    public class Transaction {
+
+        [Key]
+        public int TransactionId { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+    }
+}
